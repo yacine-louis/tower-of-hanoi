edd024ae044557af572eaaa69c0fbd6e28086b34
dilatcha <miloudiyacine2006@gmail.com>
2025-12-02 15:26:12 +0100

    updated hanoi_benchmark

--- src/hanoi_benchmark.c
+++ src/hanoi_benchmark.c
@@ -10,26 +10,38 @@ void runSingleTest()
 {
   int n, method;
 
-  printf("----- Single Test -----\n");
-  printf("Enter number of disks (1-%d): ", MAX_DISKS);
-  scanf("%d", &n);
+  printf("\n--------- Single Test ---------\n");
 
-  if (n < 1 || n > MAX_DISKS)
+  // validate number of disks, it must be integer and positive
+  while (true)
   {
-    printf("Invalid number of disks!\n");
-    return;
+    printf("Enter number of disks: ");
+    if (scanf("%d", &n) == 1 && n > 0)
+      break;
+
+    printf("invalid number of disks!\n");
+
+    // not valid number of disks, clear input buffer
+    while (getchar() != '\n')
+      ;
   }
 
   printf("\nSelect method:\n");
-  printf(" 1. Recursive\n");
-  printf(" 2. Iterative\n");
-  printf("Choice: ");
-  scanf("%d", &method);
+  printf(" 1. recursive\n");
+  printf(" 2. iterative\n");
 
-  if (method != 1 && method != 2)
+  // validate method, it must be integer and either 1 or 2
+  while (true)
   {
-    printf("Invalid method!\n");
-    return;
+    printf("Choice: ");
+    if (scanf("%d", &method) == 1 && (method == 1 || method == 2))
+      break;
+
+    printf("invalid method! enter 1 or 2\n");
+
+    // not valid method, clear input buffer
+    while (getchar() != '\n')
+      ;
   }
 
   Stack *A = createStack();
@@ -38,7 +50,7 @@ void runSingleTest()
 
   if (!A || !B || !C)
   {
-    printf("Failed to create stacks.\n");
+    printf("failed to create stacks\n");
     freeStack(A);
     freeStack(B);
     freeStack(C);
@@ -62,7 +74,7 @@ void runSingleTest()
 
   if (method == 1)
   {
-    printf("\n--- Running Recursive method ---\n");
+    printf("\n--- Running recursive method ---\n");
     hanoiRecursive(n, A, C, B);
   }
   else
@@ -89,7 +101,7 @@ void runSingleTest()
   printf("\n--- Results ---\n");
   printf("Number of disks: %d\n", n);
   printf("Method: %s\n", method == 1 ? "Recursive" : "Iterative");
-  printf("Execution time: %f (sec)", getElapsedTime(&t));
+  printf("Execution time: %.4f (sec)", getElapsedTime(&t));
 
   printf("\nPress Enter to continue...");
   getchar();
@@ -98,99 +110,129 @@ void runSingleTest()
 
 void runBenchmarkSuite()
 {
-  int method, start_n, end_n;
+  int start_n, end_n, method;
+  printf("\n--------- Range Test ---------\n");
 
-  printf("\n--- Range Test ---\n\n");
-
-  printf("Select method:\n");
-  printf(" 1. Recursive\n");
-  printf(" 2. Iterative\n");
-  printf("Choice: ");
-  scanf("%d", &method);
-
-  if (method != 1 && method != 2)
+  // validate starting number, it must be integer and positive
+  while (true)
   {
-    printf("Invalid method!\n");
-    return;
+    printf("Enter starting number of disks: ");
+    if (scanf("%d", &start_n) == 1 && start_n > 0)
+      break;
+
+    printf("invalid starting number of disks! try again...\n");
+
+    // not valid starting number, clear input buffer
+    while (getchar() != '\n')
+      ;
   }
 
-  printf("\nEnter starting number of disks (1-%d): ", MAX_DISKS);
-  scanf("%d", &start_n);
-  printf("Enter ending number of disks (%d-%d): ", start_n, MAX_DISKS);
-  scanf("%d", &end_n);
-
-  if (start_n < 1 || end_n > MAX_DISKS || start_n > end_n || (end_n - start_n + 1) > MAX_DISKS)
+  // validate ending number, it must be integer and bigger than starting number
+  while (true)
   {
-    printf("Invalid range! ");
-    if (start_n > end_n)
+    printf("Enter ending number of disks: ");
+    if (scanf("%d", &end_n) == 1)
     {
-      printf("Ending number must be greater than or equal to starting number.\n");
-    }
-    else if (start_n < 1)
-    {
-      printf("Starting number must be at least 1.\n");
-    }
-    else if (end_n > MAX_DISKS)
-    {
-      printf("Ending number must be at most %d.\n", MAX_DISKS);
+      if (end_n <= start_n)
+      {
+        printf("ending number must be greater than staring number\n");
+      }
+      else
+      {
+        break;
+      }
     }
     else
     {
-      printf("Range is too large (max %d tests).\n", MAX_DISKS);
+      printf("invalid ending number of disks! try again...\n");
     }
-    printf("\nPress Enter to continue...");
-    getchar();
-    getchar();
-    return;
+
+    // not valid starting number, clear input buffer
+    while (getchar() != '\n')
+      ;
   }
 
+  printf("Select method:\n");
+  printf(" 1. recursive\n");
+  printf(" 2. iterative\n");
+
+  // validate method, it must be integer and either 1 or 2
+  while (true)
+  {
+    printf("Choice: ");
+    if (scanf("%d", &method) == 1 && (method == 1 || method == 2))
+      break;
+
+    printf("invalid method! enter 1 or 2\n");
+
+    // not valid method, clear input buffer
+    while (getchar() != '\n')
+      ;
+  }
+
+  // structure to store the benchmark result
   BenchmarkData data;
-  data.count = 0;
+  data.results = (BenchmarkResult *)malloc((end_n - start_n + 1) * sizeof(BenchmarkResult));
+
+  // check if allocation failed
+  if (data.results == NULL)
+  {
+    printf("\nallocating benchmark results failed");
+    return;
+  }
+  // init result length
+  data.resultsLength = end_n - start_n + 1;
   strcpy(data.method, method == 1 ? "Recursive" : "Iterative");
 
   printf("\n--- Running Benchmark ---\n");
-  printf("%-8s %-15s %-15s\n", "Disks", "Moves", "Time (sec)");
-  printf("-------------------------------------------------\n");
+  printf("%-8s %-15s\n", "Disks", "Time (sec)");
+  printf("----------------------\n");
 
   for (int n = start_n; n <= end_n; n++)
   {
-    Node *A = NULL, *B = NULL, *C = NULL;
+    Stack *A = createStack();
+    Stack *B = createStack();
+    Stack *C = createStack();
+
+    if (!A || !B || !C)
+    {
+      printf("failed to create stacks\n");
+      freeStack(A);
+      freeStack(B);
+      freeStack(C);
+      return;
+    }
 
     // initialize stack A
     for (int i = n; i >= 1; i--)
-      push(&A, i);
+      push(A, i);
 
     Timer t;
     startTimer(&t);
 
     if (method == 1)
     {
-      hanoiRecursive(n, &A, &C, &B);
+      hanoiRecursive(n, A, C, B);
     }
     else
     {
-      hanoiIterative(n, &A, &B, &C);
+      hanoiIterative(n, A, B, C);
     }
 
     endTimer(&t);
-
-    long moves = (1L << n) - 1;
+    // get elapsed time
     double elapsed = getElapsedTime(&t);
+    // fill result data
+    int index = n - start_n;
+    data.results[index].n = n;
+    data.results[index].time_sec = elapsed;
 
-    data.results[data.count].n = n;
-    data.results[data.count].time_sec = elapsed;
-    data.results[data.count].moves = moves;
-    data.count++;
-
-    printf("%-8d %-15ld %-15.6f\n", n, moves, elapsed);
+    printf("%-8d %-15.4f\n", n, elapsed);
 
     // free memory
-    while (A)
-      pop(&A);
-    while (B)
-      pop(&B);
-    while (C)
-      pop(&C);
+    freeStack(A);
+    freeStack(B);
+    freeStack(C);
   }
 
   printf("\nBenchmark complete!\n");
@@ -201,4 +243,8 @@ void runBenchmarkSuite()
   printf("\nPress Enter to continue...");
   getchar();
   getchar();
+
+  // free dynamic result array
+  free(data.results);
+  data.results = NULL;
 }
