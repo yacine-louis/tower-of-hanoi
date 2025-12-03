352b0a4404d06e3d30da08927000ec68402af590
Baatout Mohamed Amine <baatoutamine5@gmail.com>
2025-12-03 17:24:37 +0100

    visualizer

--- include/hanoi_visualizer.h
+++ include/hanoi_visualizer.h
@@ -7,15 +7,16 @@
 #define SCREEN_WIDTH 1200
 #define SCREEN_HEIGHT 700
 
-// visualization functions
-
 // fill the benchmark table/graph
 void visualizeResults(BenchmarkData *data);
 
 // draw the benchmark table
-void drawTable(BenchmarkData *data, Rectangle area, Font regularFont, Font boldFont, int startIndex);
+int drawTable(BenchmarkData *data, Rectangle area, Font regularFont, Font boldFont, int scroll);
 
 // draw the benchmark graph
 void drawGraph(BenchmarkData *data, Rectangle area, Font regularFont, Font boldFont);
+
+// draw text using custom font
+void DrawTextWithFallBack(Font font, const char *text, Vector2 position, int fontSize, float spacing, Color color);
 
 #endif
--- src/hanoi_visualizer.c
+++ src/hanoi_visualizer.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include "hanoi_visualizer.h"
 
+void DrawTextWithFallBack(Font font, const char *text, Vector2 position, int fontSize, float spacing, Color color)
+{
+  // check if custom font exist
+  if (font.baseSize > 0)
+    DrawTextEx(font, text, position, fontSize, spacing, color);
+  else
+    // use default font
+    DrawText(text, (int)position.x, (int)position.y, fontSize, color);
+}
+
 void visualizeResults(BenchmarkData *data)
 {
   // set window settings
   SetTraceLogLevel(LOG_WARNING);
-  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Tower of Hanoi - Results Visualization");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Tower of Hanoi");
   SetTargetFPS(60);
 
   // load fonts
   Font regularFont = FileExists("assets/Roboto-Regular.ttf") ? LoadFontEx("assets/Roboto-Regular.ttf", 32, 0, 0) : GetFontDefault();
   Font boldFont = FileExists("assets/Roboto-Bold.ttf") ? LoadFontEx("assets/Roboto-Bold.ttf", 32, 0, 0) : regularFont;
 
-  // these are used to unload the fonts(free the memory)
+  // these are used to unload the fonts (free the memory)
   bool shouldUnloadRegular = (regularFont.texture.id != GetFontDefault().texture.id);
   bool shouldUnloadBold = (boldFont.texture.id != regularFont.texture.id) && (boldFont.texture.id != GetFontDefault().texture.id);
-
-  // index where table start
-  int tableStart = 0;
 
   // add textures to fonts so they don't look pixelated
   if (regularFont.texture.id != 0)
     SetTextureFilter(regularFont.texture, TEXTURE_FILTER_BILINEAR);
   if (boldFont.texture.id != 0 && boldFont.texture.id != regularFont.texture.id)
     SetTextureFilter(boldFont.texture, TEXTURE_FILTER_BILINEAR);
+
+  // index where table start
+  int tableScroll = 0;
 
   // screen loop
   while (!WindowShouldClose())
@@ -32,70 +42,16 @@ void visualizeResults(BenchmarkData *data)
     ClearBackground(RAYWHITE);
 
     // title and method
-    DrawTextEx(boldFont, "TOWER OF HANOI - BENCHMARK RESULTS", (Vector2){20, 20}, 24, 1, DARKBLUE);
-    char methodText[50];
-    sprintf(methodText, "Method: %s", data->method);
-    DrawTextEx(boldFont, methodText, (Vector2){20, 50}, 20, 1, DARKGRAY);
+    DrawTextWithFallBack(boldFont, "Tower Of Hanoi", (Vector2){20, 20}, 24, 1, DARKBLUE);
+    DrawTextWithFallBack(boldFont, data->method, (Vector2){20, 50}, 20, 1, DARKGRAY);
 
     // define areas
     Rectangle tableArea = {20, 90, 350, SCREEN_HEIGHT - 110};
     Rectangle graphArea = {390, 90, SCREEN_WIDTH - 410, SCREEN_HEIGHT - 110};
 
-    // Calculate visible rows
-    int rowHeight = 25;
-    int availableHeight = (int)tableArea.height - 80;
-    int maxVisibleRows = availableHeight / rowHeight;
-    int maxScroll = (data->count > maxVisibleRows) ? data->count - maxVisibleRows : 0;
-
-    // handle scrolling
-    tableStart -= (int)GetMouseWheelMove();
-    if (IsKeyPressed(KEY_DOWN))
-      tableStart++;
-    if (IsKeyPressed(KEY_UP))
-      tableStart--;
-    if (IsKeyPressed(KEY_PAGE_DOWN))
-      tableStart += maxVisibleRows;
-    if (IsKeyPressed(KEY_PAGE_UP))
-      tableStart -= maxVisibleRows;
-
-    // define scrolling bandwidth
-    if (tableStart < 0)
-      tableStart = 0;
-    if (tableStart > maxScroll)
-      tableStart = maxScroll;
-
-    // draw table and graph
-    drawTable(data, tableArea, regularFont, boldFont, tableStart);
+    tableScroll = drawTable(data, tableArea, regularFont, boldFont, tableScroll);
+    // draw graph
     drawGraph(data, graphArea, regularFont, boldFont);
-
-    // draw scrollbar if the rows exceeded the visible rows
-    if (data->count > maxVisibleRows)
-    {
-      float trackX = tableArea.x + tableArea.width - 12;
-      float trackY = tableArea.y + 80;
-      float trackW = 8;
-      float trackH = availableHeight;
-
-      // static scrollbar
-      DrawRectangle(trackX, trackY, trackW, trackH, LIGHTGRAY);
-      DrawRectangleLines(trackX, trackY, trackW, trackH, LIGHTGRAY);
-
-      // moving scrollbar
-      float visibleRatio = (float)maxVisibleRows / (float)data->count;
-      float thumbHeight = visibleRatio * trackH;
-      if (thumbHeight < 20)
-        thumbHeight = 20;
-
-      float scrollRatio = (float)tableStart / maxScroll;
-      float thumbY = trackY + (trackH - thumbHeight) * scrollRatio;
-
-      DrawRectangle(trackX + 1, thumbY, trackW - 2, thumbHeight, BLACK);
-    }
-
-    // ability to close window with esc
-    const char *escText = "Press ESC to close";
-    Vector2 escSize = MeasureTextEx(regularFont, escText, 16.0f, 1.0f);
-    DrawTextEx(regularFont, escText, (Vector2){SCREEN_WIDTH - escSize.x - 20, 12}, 16.0f, 1.0f, GRAY);
 
     EndDrawing();
   }
@@ -109,134 +65,139 @@ void visualizeResults(BenchmarkData *data)
   CloseWindow();
 }
 
-void drawTable(BenchmarkData *data, Rectangle area, Font regularFont, Font boldFont, int startIndex)
+int drawTable(BenchmarkData *data, Rectangle area, Font regularFont, Font boldFont, int scroll)
 {
-  // Check if custom fonts are loaded (a loaded font has a non-zero baseSize)
-  bool useCustomFonts = (regularFont.baseSize > 0 && boldFont.baseSize > 0);
-
-  // Table background
+  // set table background
   DrawRectangleRec(area, (Color){245, 245, 245, 255});
   DrawRectangleLinesEx(area, 2, DARKGRAY);
 
-  // Title (bold font if available)
-  if (useCustomFonts)
-    DrawTextEx(boldFont, "Results Table", (Vector2){area.x + 10, area.y + 10}, 18, 1, DARKBLUE);
-  else
-    DrawText("Results Table", area.x + 10, area.y + 10, 18, DARKBLUE);
+  // draw table title
+  DrawTextWithFallBack(boldFont, "Results Table", (Vector2){area.x + 10, area.y + 10}, 18, 1, DARKBLUE);
 
-  // Header (bold font if available)
+  // draw table header
   int headerY = area.y + 45;
-  if (useCustomFonts)
-  {
-    DrawTextEx(boldFont, "Disks", (Vector2){area.x + 20, headerY}, 16, 1, BLACK);
-    DrawTextEx(boldFont, "Moves", (Vector2){area.x + 100, headerY}, 16, 1, BLACK);
-    DrawTextEx(boldFont, "Time (sec)", (Vector2){area.x + 200, headerY}, 16, 1, BLACK);
-  }
-  else
-  {
-    DrawText("Disks", area.x + 20, headerY, 16, BLACK);
-    DrawText("Moves", area.x + 100, headerY, 16, BLACK);
-    DrawText("Time (sec)", area.x + 200, headerY, 16, BLACK);
-  }
+  DrawTextWithFallBack(boldFont, "Disks", (Vector2){area.x + 20, headerY}, 16, 1, BLACK);
+  DrawTextWithFallBack(boldFont, "Time (sec)", (Vector2){area.x + 150, headerY}, 16, 1, BLACK);
+
   DrawLine(area.x + 10, headerY + 25, area.x + area.width - 10, headerY + 25, DARKGRAY);
 
-  // Calculate visible rows area for scrolling
-  int rowY = headerY + 35;
+  // calculate visible rows
   int rowHeight = 25;
+  int rowY = headerY + 35;
   int rowsAreaY = rowY;
-  int rowsAreaHeight = (int)(area.y + area.height - rowsAreaY - 10);
+  int rowsAreaHeight = (area.y + area.height - 10) - rowsAreaY;
   if (rowsAreaHeight < rowHeight)
     rowsAreaHeight = rowHeight;
-  int visibleRows = rowsAreaHeight / rowHeight;
 
-  // Clip to rows area to enable scrolling without drawing outside bounds
+  int visibleRows = rowsAreaHeight / rowHeight;
+  int maxScroll = (data->resultsLength > visibleRows) ? data->resultsLength - visibleRows : 0;
+
+  // handle scrolling
+  scroll -= (int)GetMouseWheelMove();
+  if (IsKeyPressed(KEY_DOWN))
+    scroll++;
+  if (IsKeyPressed(KEY_UP))
+    scroll--;
+  if (IsKeyPressed(KEY_PAGE_DOWN))
+    scroll += visibleRows;
+  if (IsKeyPressed(KEY_PAGE_UP))
+    scroll -= visibleRows;
+
+  // define scroll bandwidth
+  if (scroll < 0)
+    scroll = 0;
+  if (scroll > maxScroll)
+    scroll = maxScroll;
+
+  // enable scrolling
   BeginScissorMode((int)area.x + 10, rowsAreaY - 5, (int)area.width - 20, rowsAreaHeight + 10);
 
-  // Draw each visible row
+  // draw visible rows
   for (int row = 0; row < visibleRows; row++)
   {
-    int i = startIndex + row;
-    if (i >= data->count)
+    int i = scroll + row;
+    if (i >= data->resultsLength)
       break;
 
-    // Format the data for display
-    char diskStr[10], movesStr[20], timeStr[25];
+    char diskStr[10];
+    char timeStr[20];
+
+    // format data
     sprintf(diskStr, "%d", data->results[i].n);
-    sprintf(movesStr, "%ld", data->results[i].moves);
 
-    // Format time with appropriate precision based on magnitude
+    // format time
     if (data->results[i].time_sec < 0.001)
-    {
       sprintf(timeStr, "< 0.001");
-    }
-    else if (data->results[i].time_sec < 1.0)
-    {
-      sprintf(timeStr, "%.6f", data->results[i].time_sec);
-    }
     else
-    {
-      sprintf(timeStr, "%.3f", data->results[i].time_sec);
-    }
+      sprintf(timeStr, "%.4f", data->results[i].time_sec);
 
-    // Alternate row colors for better readability
+    // set color for row
     Color rowColor = (i % 2 == 0) ? (Color){255, 255, 255, 255} : (Color){240, 240, 240, 255};
     float y = rowY + row * rowHeight;
     DrawRectangle(area.x + 10, y - 5, area.width - 20, rowHeight - 2, rowColor);
 
-    // Draw row data (using custom font if available)
-    if (useCustomFonts)
-    {
-      DrawTextEx(regularFont, diskStr, (Vector2){area.x + 20, y}, 14, 1, DARKGRAY);
-      DrawTextEx(regularFont, movesStr, (Vector2){area.x + 100, y}, 14, 1, DARKGRAY);
-      DrawTextEx(regularFont, timeStr, (Vector2){area.x + 200, y}, 14, 1, DARKGRAY);
-    }
-    else
-    {
-      DrawText(diskStr, area.x + 20, y, 14, DARKGRAY);
-      DrawText(movesStr, area.x + 100, y, 14, DARKGRAY);
-      DrawText(timeStr, area.x + 200, y, 14, DARKGRAY);
-    }
+    // draw row content
+    DrawTextWithFallBack(regularFont, diskStr, (Vector2){area.x + 20, y}, 16, 1, DARKGRAY);
+    DrawTextWithFallBack(regularFont, timeStr, (Vector2){area.x + 150, y}, 16, 1, DARKGRAY);
   }
 
   EndScissorMode();
+
+  // draw scrollbar
+  if (data->resultsLength > visibleRows)
+  {
+    int trackX = (int)(area.x + area.width - 12);
+    int trackY = (int)(area.y + 80);
+    int trackW = 8;
+    int trackH = rowsAreaHeight;
+
+    DrawRectangle(trackX, trackY, trackW, trackH, LIGHTGRAY);
+    DrawRectangleLines(trackX, trackY, trackW, trackH, LIGHTGRAY);
+
+    float ratio = (float)visibleRows / data->resultsLength;
+    int thumbH = ratio * trackH;
+    if (thumbH < 20)
+      thumbH = 20;
+
+    float posRatio = (float)scroll / maxScroll;
+    int thumbY = trackY + (int)((trackH - thumbH) * posRatio);
+
+    DrawRectangle(trackX + 1, thumbY, trackW - 2, thumbH, BLACK);
+  }
+
+  return scroll;
 }
 
 void drawGraph(BenchmarkData *data, Rectangle area, Font regularFont, Font boldFont)
 {
-  // Check if custom fonts are loaded (a loaded font has a non-zero baseSize)
-  bool useCustomFonts = (regularFont.baseSize > 0 && boldFont.baseSize > 0);
-
-  // Graph background
+  // set graph background
   DrawRectangleRec(area, (Color){245, 245, 245, 255});
   DrawRectangleLinesEx(area, 2, DARKGRAY);
 
-  // Title (bold font if available)
-  if (useCustomFonts)
-    DrawTextEx(boldFont, "Execution Time vs. Number of Disks", (Vector2){area.x + 10, area.y + 10}, 18, 1, DARKBLUE);
-  else
-    DrawText("Execution Time vs. Number of Disks", area.x + 10, area.y + 10, 18, DARKBLUE);
+  // draw graph title
+  DrawTextWithFallBack(boldFont, "Execution Time vs. Number of Disks", (Vector2){area.x + 0.75 * area.x, area.y + 10}, 18, 1, DARKBLUE);
 
-  // Need at least 2 data points to draw a graph
-  if (data->count < 2)
+  // assure that least 2 points to draw graph
+  if (data->resultsLength < 2)
     return;
 
-  // Define graph plotting area with padding for labels
+  // define graph area
   float padding = 60;
   float graphX = area.x + padding;
   float graphY = area.y + 50;
   float graphWidth = area.width - padding * 2;
   float graphHeight = area.height - padding - 60;
 
-  // Draw axes
-  DrawLine(graphX, graphY + graphHeight, graphX + graphWidth, graphY + graphHeight, BLACK); // X-axis
-  DrawLine(graphX, graphY, graphX, graphY + graphHeight, BLACK);                            // Y-axis
+  // draw x-axis and y-axis
+  DrawLine(graphX, graphY + graphHeight, graphX + graphWidth, graphY + graphHeight, BLACK);
+  DrawLine(graphX, graphY, graphX, graphY + graphHeight, BLACK);
 
-  // Find min and max values for scaling
+  // find min and max values
   double maxTime = 0;
   int minN = data->results[0].n;
   int maxN = data->results[0].n;
 
-  for (int i = 0; i < data->count; i++)
+  for (int i = 0; i < data->resultsLength; i++)
   {
     if (data->results[i].time_sec > maxTime)
       maxTime = data->results[i].time_sec;
@@ -246,10 +207,10 @@ void drawGraph(BenchmarkData *data, Rectangle area, Font regularFont, Font boldF
       maxN = data->results[i].n;
   }
 
-  // Add 10% padding to max for better visualization
-  maxTime *= 1.1;
+  if (maxTime < 0.001)
+    maxTime = 0.001;
 
-  // Draw horizontal grid lines and Y-axis labels
+  // draw graph lines and the time in y-axis
   int numYGridLines = 5;
   for (int i = 0; i <= numYGridLines; i++)
   {
@@ -260,56 +221,42 @@ void drawGraph(BenchmarkData *data, Rectangle area, Font regularFont, Font boldF
     char label[20];
     sprintf(label, "%.2f", value);
 
-    if (useCustomFonts)
-      DrawTextEx(regularFont, label, (Vector2){graphX - 50, y - 8}, 12, 1, DARKGRAY);
-    else
-      DrawText(label, graphX - 50, y - 8, 12, DARKGRAY);
+    DrawTextWithFallBack(regularFont, label, (Vector2){graphX - 50, y - 8}, 14, 1, DARKGRAY);
   }
 
-  // Draw X-axis labels (number of disks)
-  for (int i = 0; i < data->count; i++)
+  // draw number of disks in x_axis
+  for (int i = 0; i < data->resultsLength; i++)
   {
-    float x = graphX + (i * graphWidth / (data->count - 1));
+    float x = graphX + (i * graphWidth / (data->resultsLength - 1));
     char label[10];
     sprintf(label, "%d", data->results[i].n);
 
-    if (useCustomFonts)
-      DrawTextEx(regularFont, label, (Vector2){x - 10, graphY + graphHeight + 10}, 12, 1, DARKGRAY);
-    else
-      DrawText(label, x - 10, graphY + graphHeight + 10, 12, DARKGRAY);
+    DrawTextWithFallBack(regularFont, label, (Vector2){x - 10, graphY + graphHeight + 10}, 14, 1, DARKGRAY);
   }
 
-  // Axis labels (bold font if available)
-  if (useCustomFonts)
-  {
-    DrawTextEx(boldFont, "Time (sec)", (Vector2){area.x + 5, area.y + 25}, 14, 1, DARKGRAY);
-    DrawTextEx(boldFont, "Number of Disks", (Vector2){area.x + area.width / 2 - 60, area.y + area.height - 20}, 14, 1, DARKGRAY);
-  }
-  else
-  {
-    DrawText("Time (sec)", area.x + 5, area.y + 25, 14, DARKGRAY);
-    DrawText("Number of Disks", area.x + area.width / 2 - 60, area.y + area.height - 20, 14, DARKGRAY);
-  }
+  // draw labels from x-axis and y-axis
+  DrawTextWithFallBack(boldFont, "Time (sec)", (Vector2){area.x + 5, area.y + 25}, 14, 1, DARKGRAY);
+  DrawTextWithFallBack(boldFont, "Number of Disks", (Vector2){area.x + area.width / 2 - 60, area.y + area.height - 20}, 14, 1, DARKGRAY);
 
-  // Plot data points and connecting lines
-  for (int i = 0; i < data->count - 1; i++)
+  // draw graph points and connect them
+  for (int i = 0; i < data->resultsLength - 1; i++)
   {
-    // Calculate positions for current and next points
-    float x1 = graphX + (i * graphWidth / (data->count - 1));
+    // calculate positions for current and next points
+    float x1 = graphX + (i * graphWidth / (data->resultsLength - 1));
     float y1 = graphY + graphHeight - (data->results[i].time_sec / maxTime * graphHeight);
 
-    float x2 = graphX + ((i + 1) * graphWidth / (data->count - 1));
+    float x2 = graphX + ((i + 1) * graphWidth / (data->resultsLength - 1));
     float y2 = graphY + graphHeight - (data->results[i + 1].time_sec / maxTime * graphHeight);
 
-    // Draw line connecting points
+    // draw line connecting the 2 points
     DrawLineEx((Vector2){x1, y1}, (Vector2){x2, y2}, 2, BLUE);
 
-    // Draw data point
+    // draw data point
     DrawCircle(x1, y1, 5, RED);
   }
 
-  // Draw the last data point
-  float lastX = graphX + ((data->count - 1) * graphWidth / (data->count - 1));
-  float lastY = graphY + graphHeight - (data->results[data->count - 1].time_sec / maxTime * graphHeight);
+  // draw the last data point
+  float lastX = graphX + ((data->resultsLength - 1) * graphWidth / (data->resultsLength - 1));
+  float lastY = graphY + graphHeight - (data->results[data->resultsLength - 1].time_sec / maxTime * graphHeight);
   DrawCircle(lastX, lastY, 5, RED);
 }
